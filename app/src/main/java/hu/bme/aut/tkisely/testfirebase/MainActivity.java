dbc6182231f8454836242b4c2ed2cce3e5c2d256
kiselytamas <tamaskisely@gmail.com>
2017-12-07 01:51:47 +0100

    FireBase dependencies added

--- app/src/main/java/hu/bme/aut/tkisely/testfirebase/MainActivity.java
+++ app/src/main/java/hu/bme/aut/tkisely/testfirebase/MainActivity.java
@@ -3,11 +3,18 @@ package hu.bme.aut.tkisely.testfirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
